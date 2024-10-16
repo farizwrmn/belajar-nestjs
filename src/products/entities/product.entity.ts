96d00dd6669459d7db557e096623061f926b898d
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-10-16 10:54:03 +0700

    initial commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Category } from '../../categories/entities/category.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @ManyToOne(() => Category, (category) => category.products)
+  category: Category;
+}
